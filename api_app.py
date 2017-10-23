c36c70c8988c115a97347cd274974a66679a3232
gustavwennerblom <gustav.wennerblom@gmail.com>
2017-10-23 21:33:06 +0200

    Dead end with two dbs

--- api_app.py
+++ api_app.py
@@ -35,36 +35,43 @@ connstring_css = 'mysql+mysqlconnector://' + \
 
 # Connect to database #1 and hook SQLAlchemy to it
 Base_gppt = declarative_base()
-engine = create_engine(connstring_gppt)
+engine_gppt = create_engine(connstring_gppt)
 logging.info("Connection to database established")
-Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-mysession = scoped_session(Session)
-metadata = MetaData(bind=engine)
+Session_gppt = sessionmaker(autocommit=False, autoflush=False, bind=engine_gppt)
+mysession_gppt = scoped_session(Session_gppt)
+metadata_gppt = MetaData(bind=engine_gppt)
+
+# Connect to database #1 and hook SQLAlchemy to it
+Base_css = declarative_base()
+engine_css = create_engine(connstring_css)
+logging.info("Connection to database established")
+Session_css = sessionmaker(autocommit=False, autoflush=False, bind=engine_css)
+mysession_css = scoped_session(Session_css)
+metadata_css = MetaData(bind=engine_css)
 
 
-
-# Create metadata from existing database schema
-# engine.reflect()
-# logging.info("Database metadata reflected")
 
 # Define model
 class GPPTSubmission(Base_gppt):
     __tablename__ = 'gppt_submissions'
-    __table__ = Table('gppt_submissions', metadata, autoload=True)
+    __table__ = Table('gppt_submissions', metadata_gppt, autoload=True)
     logging.info("Model for gppt_submissions created")
 
 class CSATProject(Base_gppt):
     __tablename__ = 'projects'
-
+    __table__ = Table('projects', metadata_css, autoload=True)
+    logging.info("Model for surveyed projects created")
 
 
 # Apply model and instantiate ORM
 logging.info("Database models applied")
 
 # Create APIs and delimit transferring the actual binary file data
-manager = flask_restless.APIManager(api_app, session=mysession)
-manager.create_api(GPPTSubmission, methods=['GET'], exclude_columns=['Attachment_Binary'])
+manager_gppt = flask_restless.APIManager(api_app, session=mysession_gppt)
+manager_gppt.create_api(GPPTSubmission, methods=['GET'], exclude_columns=['Attachment_Binary'])
 logging.info('Endpoint "<base_uri>/api/gppt_submissions" set up')
+manager_css = flask_restless.APIManager(api_app, session=mysession_css)
+manager_css.create_api(CSATProject, method=['GET'])
 
 # Usage:
 # Get data on one specific submission from enpoint 'http://localhost:5000/api/gppt_submissions/1'
